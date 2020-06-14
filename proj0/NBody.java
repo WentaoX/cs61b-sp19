4326b6fe195dfca6999207eb882244a6aab941cb
WentaoX <bfodxwt@gmail.com>
2020-06-14 18:17:17 +0800

    add readBodies method.

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -5,4 +5,24 @@ public class NBody {
         double radius = in.readDouble();
         return radius;
     }
+
+    public static Body[] readBodies(String fileName) {
+        In in = new In(fileName);
+        int index=0;
+        int planets_n = in.readInt();
+        Body[] planets = new Body[planets_n];
+        double radius = in.readDouble();
+        while (index < planets_n) {
+            double xx = in.readDouble();
+            double yy = in.readDouble();
+            double xv = in.readDouble();
+            double yv = in.readDouble();
+            double mass = in.readDouble();
+            String img = in.readString();
+            Body b = new Body(xx, yy, xv, yv, mass, img);
+            planets[index] = b;
+            index ++;
+            }
+        return planets;
+    }
 }
