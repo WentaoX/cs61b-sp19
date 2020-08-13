b5258d0c2f6057f6942ba199b8aad80910ffc7c3
WentaoX <bfodxwt@gmail.com>
2020-08-13 10:38:22 +0800

    add GuitarString.java.

--- hw1/es/datastructur/synthesizer/GuitarString.java
+++ hw1/es/datastructur/synthesizer/GuitarString.java
@@ -1,5 +1,6 @@
 package es.datastructur.synthesizer;
 
+
 //Note: This file will not compile until you complete task 1 (BoundedQueue).
 public class GuitarString {
     /** Constants. Do not change. In case you're curious, the keyword final
@@ -16,6 +17,11 @@ public class GuitarString {
         //       cast the result of this division operation into an int. For
         //       better accuracy, use the Math.round() function before casting.
         //       Your buffer should be initially filled with zeros.
+        int capacity = (int) (Math.round(SR / frequency));
+        buffer = new ArrayRingBuffer<Double>(capacity);
+        for (int i = 0; i < capacity; i++) {
+            buffer.enqueue(0.0);
+        }
     }
 
 
@@ -27,6 +33,13 @@ public class GuitarString {
         //
         //       Make sure that your random numbers are different from each
         //       other.
+        while (!buffer.isEmpty()) {
+            buffer.dequeue();
+        }
+        while(!buffer.isFull()) {
+            double r = Math.random() - 0.5;
+            buffer.enqueue(r);
+        }
     }
 
     /* Advance the simulation one time step by performing one iteration of
@@ -36,12 +49,15 @@ public class GuitarString {
         // TODO: Dequeue the front sample and enqueue a new sample that is
         //       the average of the two multiplied by the DECAY factor.
         //       Do not call StdAudio.play().
+        double front = buffer.dequeue();
+        double new_element = (front + buffer.peek()) * 0.5 * DECAY;
+        buffer.enqueue(new_element);
     }
 
     /* Return the double at the front of the buffer. */
     public double sample() {
         // TODO: Return the correct thing.
-        return 0;
+        return buffer.peek();
     }
 }
     // TODO: Remove all comments that say TODO when you're done.
