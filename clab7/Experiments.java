11640ed3be03a226edf8aa0d294cf6bbb8693bf3
WentaoX <bfodxwt@gmail.com>
2020-09-13 08:42:14 +0800

    add experiment of average depth compare.

--- clab7/Experiments.java
+++ clab7/Experiments.java
@@ -1,8 +1,41 @@
+import org.knowm.xchart.XYChart;
+import org.knowm.xchart.XYChartBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.knowm.xchart.SwingWrapper;
+
+
 /**
  * Created by hug.
  */
 public class Experiments {
+    private static int targetSize = 10;
+//    insert 5000 random items into a BST, and make plot of average depth
     public static void experiment1() {
+        BST<Integer> bst = new BST<>();
+
+        List<Integer> myBSTAvgDepth = new ArrayList<>();
+        List<Integer> optiBSTAvgDepth = new ArrayList<>();
+        List<Integer> nNodes = new ArrayList<>();
+
+        RandomGenerator rg = new RandomGenerator();
+        int size = 0;
+        while (bst.size() < targetSize) {
+            int next = rg.getRandomInt(100000);
+            if (!bst.contains(next)) {
+                size ++;
+                bst.add(next);
+                nNodes.add(size);
+                optiBSTAvgDepth.add(ExperimentHelper.optimalIPL(size));
+                myBSTAvgDepth.add(bst.averageDepth());
+            }
+        }
+        XYChart chart = new XYChartBuilder().width(800).height(600).xAxisTitle("nodes number").yAxisTitle("average depth").build();
+        chart.addSeries("optimal avg depth", nNodes, optiBSTAvgDepth);
+        chart.addSeries("my avg depth", nNodes, myBSTAvgDepth);
+
+        new SwingWrapper(chart).displayChart();
     }
 
     public static void experiment2() {
@@ -12,5 +45,6 @@ public class Experiments {
     }
 
     public static void main(String[] args) {
+        experiment1();
     }
 }
